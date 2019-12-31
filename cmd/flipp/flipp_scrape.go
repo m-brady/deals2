4334c13bdf915acf1b3ef246c9272055868fd050
Michael <mbrady@eyereturn.com>
2019-12-30 19:20:15 -0500

    wip

--- cmd/flipp/flipp_scrape.go
+++ cmd/flipp/flipp_scrape.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/m-brady/deals/pkg/flipp"
-	"log"
+	"github.com/m-brady/deals/pkg/flyers/mysql"
 )
 
 const SHOPPERS = 208
@@ -12,12 +14,21 @@ func main() {
 
 	response := flipp.GetFlyers()
 	flyers := response.Flyers
+	db, err := sql.Open("mysql", "root:brady@/optimum")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	flyerService := mysql.FlyerService{DB: db}
 
 	for _, flyer := range flyers {
-		if flyer.MerchantId == 208 || flyer.MerchantId == 2018 {
-
+		if flyer.MerchantId == SHOPPERS || flyer.MerchantId == LOBLAWS {
+			f, err := flyerService.Flyer(flyer.Id)
+			if err != nil {
+				flyerService.AddFlyer(&flyer)
+			}
+			fmt.Println(f)
 		}
 	}
-
-	log.Println(flyers)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module brady.dev/deals
+
+go 1.13
+
+require github.com/m-brady/deals v0.0.0-20191230230948-ffd067f7c080 // indirect
